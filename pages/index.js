b9c144a6e3d4bfe5bbe785fea02ef8828c9d2c41
Abdallah Abedraba <aabedraba@gmail.com>
2021-01-22 23:16:13 +0100

    Add camera API

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,46 @@
+import { defineCustomElements } from "@ionic/pwa-elements/loader";
+import { Plugins, CameraResultType } from "@capacitor/core";
+import { useEffect, useState } from "react";
+
+const { Camera } = Plugins;
+
 const Index = () => {
-  return <h1>Time: {new Date().toLocaleString()}</h1>;
+  const [imagePath, setImagePath] = useState("");
+
+  useEffect(() => {
+    defineCustomElements(window);
+  }, []);
+
+  async function takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+    });
+
+    setImagePath(image.webPath);
+  }
+
+  if (imagePath !== "") {
+    return (
+      <div>
+        <img src={imagePath} />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          takePicture();
+        }}
+      >
+        Take photo
+      </button>
+      ;
+    </div>
+  );
 };
 
 export default Index;
